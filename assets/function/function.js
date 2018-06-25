3d5e94c6a2dd585179bc578c0d544c67674b3055
emerald <ach.aayush.111@gmail.com>
2018-06-25 22:25:23 +0545

    update

---
+++ assets/function/function.js
@@ -0,0 +1,32 @@
+let object={
+    num:[],
+    options:[]
+}
+function getOpt(){
+    let opt1=Math.floor(Math.random()*10);
+    let opt2=Math.floor(Math.random()*10);
+    let opt3=num1*num2;
+
+    // show options
+}
+function gameStart (){
+    object.num=[];
+    object.options=[];
+// get a random number
+let num1=Math.floor(Math.random()*10);
+//get the other random number
+let num2=Math.floor(Math.random()*10);
+  // show options
+
+ getOpt(num1,num2);
+
+ //add event listener
+ document.querySelector('opt1').addEventListener('click',()=>{
+     
+ })
+}
+
+
+
+
+gameStart();
