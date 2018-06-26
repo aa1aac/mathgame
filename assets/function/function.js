ad383f192ffeb3ef90e9797e711e98c0aa0da625
emerald <ach.aayush.111@gmail.com>
2018-06-26 22:18:26 +0545

    increased functionality

--- assets/function/function.js
+++ assets/function/function.js
@@ -1,32 +1,35 @@
-let object={
+
+gameStart();
+var object={
     num:[],
     options:[]
 }
 function getOpt(){
-    let opt1=Math.floor(Math.random()*10);
-    let opt2=Math.floor(Math.random()*10);
-    let opt3=num1*num2;
-
+    num1=parseInt(document.querySelector('.num1').textContent);
+    num2=parseInt(document.querySelector('.num2').textContent);
+    var opt1=Math.floor(Math.random()*10);
+    var opt2=Math.floor(Math.random()*10);
+    var opt3=num1*num2;
+    
     // show options
 }
 function gameStart (){
-    object.num=[];
-    object.options=[];
 // get a random number
-let num1=Math.floor(Math.random()*10);
+var num1=Math.floor(Math.random()*10);
 //get the other random number
-let num2=Math.floor(Math.random()*10);
-  // show options
-
+var num2=Math.floor(Math.random()*10);
+  // show numbers
+  console.log(num1,num2);
+   document.querySelector('.num1').textContent=num1;
+   document.querySelector('.num2').textContent=num2;
  getOpt(num1,num2);
 
  //add event listener
- document.querySelector('opt1').addEventListener('click',()=>{
+//  document.querySelector('opt1').addEventListener('click',function(){
      
- })
+//  })
 }
 
 
 
 
-gameStart();
